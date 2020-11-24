5887ff1ec80494a1001491be01b0cd7387f275a7
Munevver AKBEY <munevver.akbey@gmail.com>
2020-11-24 21:15:28 +0300

    upd

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="3910f5e8-a0a4-4a22-a5e0-f337b8fedc42", modulePackage="com.konumsal.orbisozetmobil", sdkRoot="C:\\Users\\busrakbey\\AppData\\Local\\Android\\Sdk",layoutInfoDir="D:\\OrbisMobilDepoSon\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=15,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
